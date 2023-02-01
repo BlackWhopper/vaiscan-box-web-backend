a4393d816f614602437ba8ab41a0148c1c609bd7
devick <devick99@gmail.com>
2023-02-01 20:01:55 +0900

    File upload test code

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -6,6 +6,8 @@ import {
   UploadedFile,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import * as formidable from 'formidable';
+import { Req } from '@nestjs/common/decorators';
 
 @Controller('upload')
 export class UploadController {
@@ -15,5 +17,18 @@ export class UploadController {
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
     return await this.uploadService.uploadFile(file);
+  }
+
+  @Post('test')
+  async uploadTest(@Req() req) {
+    const form = new formidable.IncomingForm();
+
+    form.parse(req, (err, fields, files) => {
+      if (err) throw err;
+      console.log('\n-----------');
+      console.log('Fields', fields);
+      console.log('Received:', files);
+      console.log();
+    });
   }
 }
